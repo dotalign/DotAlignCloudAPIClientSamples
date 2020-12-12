d615ae4cfb62cc5aefad361de52d46eaf13b737f
Jaspreet Bakshi <jaspreet@dotalign.com>
2020-12-12 01:03:35 -0500

    Added some clarifying comments

--- app_iterateThroughContributors.js
+++ app_iterateThroughContributors.js
@@ -2,8 +2,18 @@ const helpers = require("./dotaligncloud/helpers");
 const iterator = require("./fetchers/iterateContributors");
 
 async function main() {
+  
   var environment = await helpers.getEnvironmentParams();
-  var result = iterator.run(environment, 1, 2, 100, 100);
+  
+  // This will iterate through data contributors and fetch 
+  // people and companies for each of them
+  
+  var result = iterator.run(
+      environment, 
+      1, // teamNumber
+      2, // the number of "data conributors" to fetch for
+      100, // the number of person records to fetch
+      100); // the number of company records to fetch
 }
 
 main();
